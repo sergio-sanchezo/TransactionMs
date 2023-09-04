ab0da2e20e7c6b0a7c58930f7ef8c0f6947d8d35
Sergio Sanchez <chechoo.alejandro@gmail.com>
2023-09-03 23:05:15 -0500

    Changed transaction controller to arrow functions

--- src/transaction/infraestructure/controller/transaction.ctrl.ts
+++ src/transaction/infraestructure/controller/transaction.ctrl.ts
@@ -2,33 +2,28 @@ import { Request, Response } from "express";
 import { TransactionUseCase } from "../../application/transactionUseCase";
 
 export class TransactionController {
-  constructor(private readonly transactionUseCase: TransactionUseCase) {
-    this.getCtrl = this.getCtrl.bind(this);
-    this.postChargeCtrl = this.postChargeCtrl.bind(this);
-    this.postP2PCtrl = this.postP2PCtrl.bind(this);
-    this.listTransactionsCtrl = this.listTransactionsCtrl.bind(this);
-  }
+  constructor(private readonly transactionUseCase: TransactionUseCase) {}
 
-  public async getCtrl(req: Request, res: Response) {
+  public getCtrl = async (req: Request, res: Response) => {
     const { id } = req.params;
     const transaction = await this.transactionUseCase.getDetailTransaction(id);
     res.status(200).json(transaction);
-  }
+  };
 
-  public async postChargeCtrl(req: Request, res: Response) {
+  public postChargeCtrl = async (req: Request, res: Response) => {
     const transaction = await this.transactionUseCase.chargeAccount(req.body);
     res.status(200).json(transaction);
-  }
+  };
 
-  public async postP2PCtrl(req: Request, res: Response) {
+  public postP2PCtrl = async (req: Request, res: Response) => {
     const transaction = await this.transactionUseCase.makeTransactionP2P(
       req.body
     );
     res.status(200).json(transaction);
-  }
+  };
 
-  public async listTransactionsCtrl(req: Request, res: Response) {
+  public listTransactionsCtrl = async (req: Request, res: Response) => {
     const transactions = await this.transactionUseCase.listTransactions();
     res.status(200).json(transactions);
-  }
+  };
 }
